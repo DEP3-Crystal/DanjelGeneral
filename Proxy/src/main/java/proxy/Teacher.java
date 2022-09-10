d2fe0dd945694ad1010475c4f04067cfc9afcf9e
danjel137 <109280898+danjel137@users.noreply.github.com>
2022-09-10 16:43:18 +0200

    producer consumer pattern

--- Proxy/src/main/java/proxy/Teacher.java
+++ Proxy/src/main/java/proxy/Teacher.java
@@ -4,9 +4,13 @@ import java.util.Date;
 
 public class Teacher {
     public static void main(String[] args) {
-        Student student=new Student();
+        Student student=new Student(new Attendance(false,new Date()));
         student.attendLesson();
-        Student studentProxy=new StudentProxy(new Attendance(true,new Date()));
-        studentProxy.attendLesson();
+
+//        Student studentProxy=new StudentProxy(new Attendance(true,new Date()));
+//        studentProxy.attendLesson();
+//
+//        DailySession dailySession=new StudentProxy(new Attendance(false,new Date()));
+//        dailySession.attendLesson();
     }
 }
